64ffa25c27c22628b178b1983f1a57eafc453cdc
ab75173 <adam.p.barrett@gmail.com>
2014-12-09 00:29:25 -0500

    added map to contact page

--- wp-content/themes/3tandfriends/front-page.php
+++ wp-content/themes/3tandfriends/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <!-- Main jumbotron for a primary marketing message or call to action
     <div class="jumbotron">
       <div class="container">
 
@@ -11,26 +11,38 @@
         <?php endwhile; endif; ?>
 
       </div>
-    </div>
+    </div> -->
 
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+
+    <!-- Adam's custom video div -->
+
+      <div class="intro">
+        <video autoplay loop muted poster="images/watch.mp4" id="bgvid">
+          <source src="../3tandfriends/wp-content/themes/3tandfriends/images/watch.mp4" type="video/mp4">
+        </video>
       </div>
+
+      <div class="container">
+        <h2>News</h2>
+        <div class="row">
+          <div class="col-md-6">
+
+            <?php
+              $args = array( 'post_type' => 'post' );
+              $the_query = new WP_Query( $args );
+              ?>
+
+            <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+              <h3><?php the_title(); ?></h3>
+              <p><?php the_content(); ?></p>
+              <hr>
+
+            <?php endwhile; else: ?>
+              <p>There are no posts or pages here</p>
+            <?php endif; ?>
+          </div>
+        </div>
+
 
 <?php get_footer(); ?>
